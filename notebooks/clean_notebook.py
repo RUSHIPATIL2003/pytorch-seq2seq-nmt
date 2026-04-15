282b45f3a670e86e64e0ac3689a79a2849adc693
Rushikesh Patil <rushipatil2501@gmail.com>
2026-04-16 01:27:45 +0530

    Fixed Notebook by removing 'widgets' from metadata

--- notebooks/clean_notebook.py
+++ notebooks/clean_notebook.py
@@ -1,11 +1,13 @@
 import nbformat
 
-file_path = "Seq2Seq_translator_model.ipynb"  # change this
+file_path = "Seq2Seq_translator_model.ipynb"
 
 nb = nbformat.read(file_path, as_version=4)
 
 if "widgets" in nb.get("metadata", {}):
     del nb["metadata"]["widgets"]
 
-nbformat.write(nb, "cleaned_notebook.ipynb")
-print("Done!")
+# overwrite the same file (no new notebook created)
+nbformat.write(nb, file_path)
+
+print("Fixed Notebook by removing 'widgets' from metadata.")
